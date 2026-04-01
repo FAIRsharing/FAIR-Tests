b5292e5d9641995585a9612089b5acd4fd4af018
Milo Thurston <knirirr@gmail.com>
2026-04-01 15:45:26 +0100

    Added a list of available tests.

--- test/static_test.rb
+++ test/static_test.rb
@@ -9,4 +9,14 @@ class StaticTest < Minitest::Test
     body = JSON.parse(last_response.body)
     assert_equal 'Welcome to the FAIR Tests API', body['message']
   end
+
+  def test_list_tests_returns_list_of_tests
+    get '/list_tests'
+
+    assert last_response.ok?
+
+    body = JSON.parse(last_response.body)
+    assert_equal 'List of available tests. Prepend /test/ to run a test and access with POST.', body['message']
+    assert_equal body['tests'].count, Dir.entries('./lib/fair_tests').reject { |f| f.start_with?('.') }.count
+  end
 end
