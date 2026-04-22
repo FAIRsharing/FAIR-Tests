aedc20aefff18d74bb09d34e68b633ecbfcee53e
Ramon Granell <ragrator@gmail.com>
2026-04-22 15:33:07 +0100

    modifying files

--- test/ft_ark_f1gupr_test.rb
+++ test/ft_ark_f1gupr_test.rb
@@ -5,9 +5,9 @@ require_relative '../lib/fair_tests/ft_ark_f1gupr'
 
 class FtArkf1guprTest < Minitest::Test
   include ::TestHelper
-  include ::FtArkF1gupr
+  include ::FtArkF1gupri
 
-  def test_pass_ft_ark_f1gupr
+  def test_pass_ft_ark_f1gupri
     stub_request(:post, "#{ENV['FAIRSHARING_API_URL']}").
       with(headers: headers).to_return(
       status: 200,
@@ -45,7 +45,7 @@ class FtArkf1guprTest < Minitest::Test
     assert_equal body['value'], 'pass'
   end
 
-  def test_fail_ft_ark_f1gupr
+  def test_fail_ft_ark_f1gupri
     stub_request(:post, "#{ENV['FAIRSHARING_API_URL']}").
       with(headers: headers).to_return(
       status: 200,
@@ -82,7 +82,7 @@ class FtArkf1guprTest < Minitest::Test
     assert_equal body['value'], 'fail'
   end
 
-  def test_indeterminate_ft_ark_f1gupr
+  def test_indeterminate_ft_ark_f1gupri
     stub_request(:post, "#{ENV['FAIRSHARING_API_URL']}").
       with(headers: headers).to_return(
       status: 200,
