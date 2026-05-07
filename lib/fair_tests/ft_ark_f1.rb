ecb4309e5382ed1f66815ba93e8b8a9ea944aa8d
Milo Thurston <knirirr@gmail.com>
2026-05-07 11:19:13 +0100

    Added ORCID. #49

--- lib/fair_tests/ft_ark_f1.rb
+++ lib/fair_tests/ft_ark_f1.rb
@@ -11,7 +11,7 @@ module FtArkF1
       testname: 'FAIR Test - ARK T-F1 - Persistent Identifiers for Database Content',
       description: 'This test checks that the database requires the minting of persistent identifiers for at least some of the content within the database being evaluated.',
       keywords: ['ARK', 'FAIR', 'F1', 'GUPRI', 'persistent identifiers'],
-      creator: '', # TODO: Add ORCID ID for Ramon
+      creator: 'https://orcid.org/0000-0001-9572-0972',
       indicators: [],
       metric: '', # TODO: Find which metric belongs here.
       license: 'https://creativecommons.org/licenses/by/4.0/',
@@ -20,16 +20,6 @@ module FtArkF1
       host: "fair-tests.fairsharing.org",
       basePath: "test"
     }
-
-    #data_test = {
-    #  test_title_short: 'FAIR Test - ARK T-F1 - Persistent Identifiers for Database Content',
-    #  test_title: 'Output from running test: FAIR Test - ARK T-F1 - Persistent Identifiers for Database Content (https://tools.ostrails.eu/fdp-index/entry/210ea450-9e80-4559-9e40-a2c8df242728)',
-    #  test_id: 'https://ostrails.github.io/assessment-component-metadata-records/test/FTARKTF1.ttl',
-    #  description: 'This test checks that the database requires the minting of persistent identifiers for at least some of the content within the database being evaluated.',
-    #  endpointDescription: 'https://fair-tests.fairsharing.org/test_descriptions/ft_ark_f1/api',
-    #  endpointURL: 'https://fair-tests.fairsharing.org/test/ft_ark_f1',
-    #  url_record: url_record
-    #}
 
     response = FtrRuby::Output.new(
       testedGUID: url_record,
