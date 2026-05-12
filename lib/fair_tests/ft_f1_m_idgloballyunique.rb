5027817b05ba769dd2e31cdb4554f2acf2f88930
knirirr <knirirr@users.noreply.github.com>
2026-05-12 15:43:48 +0100

    Update lib/fair_tests/ft_f1_m_idgloballyunique.rb
    Co-authored-by: Allyson Lister <allyson.lister@oerc.ox.ac.uk>

--- lib/fair_tests/ft_f1_m_idgloballyunique.rb
+++ lib/fair_tests/ft_f1_m_idgloballyunique.rb
@@ -10,7 +10,7 @@ module FtF1MIdgloballyunique
       testid: 'ft_f1_m_idgloballyunique',
       testname: 'FAIR Test - F1 - Metadata - evaluate identifier global uniqueness',
       description: "F1 requires that metadata be assigned a globally unique, persistent and resolvable identifier. This test is particularly concerned with the globally uniqueness aspect of GUPRIs; global uniqueness is required to prevent identifier collision across institutions, systems and domains. Otherwise, an identifier shared by multiple resources will confound efforts to describe that resource, or to use the identifier to retrieve it. In this test, the definition of global uniqueness follows the FAIRsharing guidance on Globally Unique, Persistent and Resolvable Identifier (GUPRI) schemas. The identifier being evaluated is checked for a match (using regular expressions) to an existing id schema within FAIRsharing. Pass: There is an id_schema record matching the regular expression that has the globally unique field set to 'true'. Indeterminate: No matching records were found in FAIRsharing. Fail: No indication of this resource having a globally unique identifier was found.",
-      keywords: ['FAIR', 'A2', 'GUPRI', 'globally unique identifiers'],
+      keywords: ['FAIR', 'F1', 'GUID', 'globally unique identifiers'],
       creator: 'https://orcid.org/0000-0002-6468-9260',
       indicators: [],
       metric: 'https://doi.org/10.25504/FAIRsharing.0ad027',
