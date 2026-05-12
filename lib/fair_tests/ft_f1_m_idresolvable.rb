4f9659dc509b823696391c97dcb90018ab7c09a6
knirirr <knirirr@users.noreply.github.com>
2026-05-12 15:24:54 +0100

    Update lib/fair_tests/ft_f1_m_idresolvable.rb
    Co-authored-by: Allyson Lister <allyson.lister@oerc.ox.ac.uk>

--- lib/fair_tests/ft_f1_m_idresolvable.rb
+++ lib/fair_tests/ft_f1_m_idresolvable.rb
@@ -9,7 +9,7 @@ module FtF1MIdresolvable
       testid: 'ft_f1_m_idresolvable',
       testname: 'FAIR Test – F1 – Metadata - Evaluate identifier resolvability',
       description: "F1 requires that metadata be assigned a globally unique, persistent and resolvable identifier. This test is particularly concerned with the resolvability aspect of GUPRIs. Resolvability is required so that identifiers are actionable for both humans and machines. In this test, the definition of resolvability follows the FAIRsharing guidance on Globally Unique, Persistent and Resolvable Identifier (GUPRI) schemas. The identifier being evaluated is checked for a match (using regular expressions) to an existing id schema within FAIRsharing. Pass: There is an id_schema record matching the regular expression that has the resolvable field set to 'true'. Indeterminate: No matching records were found in FAIRsharing. Fail: No indication of this resource having a resolvable identifier was found.",
-      keywords: ['FAIR', 'A2', 'GUPRI', 'globally unique identifiers'],
+      keywords: ['FAIR', 'F1', , 'resolvable identifiers'],
       creator: 'https://orcid.org/0000-0002-6468-9260',
       indicators: [],
       metric: 'https://doi.org/10.25504/FAIRsharing.f8508f'
