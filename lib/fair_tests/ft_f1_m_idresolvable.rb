3c964b15a99197b338a490abb78a8b6269d59c0e
knirirr <knirirr@users.noreply.github.com>
2026-05-12 15:24:40 +0100

    Update lib/fair_tests/ft_f1_m_idresolvable.rb
    Co-authored-by: Allyson Lister <allyson.lister@oerc.ox.ac.uk>

--- lib/fair_tests/ft_f1_m_idresolvable.rb
+++ lib/fair_tests/ft_f1_m_idresolvable.rb
@@ -12,7 +12,7 @@ module FtF1MIdresolvable
       keywords: ['FAIR', 'A2', 'GUPRI', 'globally unique identifiers'],
       creator: 'https://orcid.org/0000-0002-6468-9260',
       indicators: [],
-      metric: '', # TODO: Add FAIRsharing metric for this test.
+      metric: 'https://doi.org/10.25504/FAIRsharing.f8508f'
       license: 'https://creativecommons.org/licenses/by/4.0/',
       testversion: '1.0.0',
       protocol: 'https',
