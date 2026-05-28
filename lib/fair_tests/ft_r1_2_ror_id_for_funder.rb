40d85cd88725c4c8bd4c475ef9e5cd8afe5d2dc9
ramGranell <39826987+ramGranell@users.noreply.github.com>
2026-05-28 14:33:22 +0100

    Update test name for ROR ID for funder

--- lib/fair_tests/ft_r1_2_ror_id_for_funder.rb
+++ lib/fair_tests/ft_r1_2_ror_id_for_funder.rb
@@ -8,7 +8,7 @@ module FtR12RorIdForFunder
 
     meta = {
       testid: 'ft_r1_2_ror_id_for_funder',
-      testname: 'FAIR Test – R1.2 - ROR ID for funder',
+      testname: 'FAIR Test - R1.2 - Metadata - ROR ID for funder',
       description: 'This test evaluates whether the metadata includes at least one ROR ID for a funder associated with the research object. The assessment checks for structured funding references within both the landing page metadata and the central records held by DOI registration agencies (such as DataCite or Crossref). Specifically, it verifies that fields such as fundingReferences (DataCite) or funder (Crossref) are populated with at least one funder ROR ID. It checks the record’s landing page for embedded or linked structured data that can be successfully parsed against the declared community schema. If the record contains ROR identifiers it will pass; otherwise, the test will fail.',
       keywords: ['FAIR', 'R1.2', 'ror id'],
       creator: 'https://orcid.org/0000-0002-6468-9260',
