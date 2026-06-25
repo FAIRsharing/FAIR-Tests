53c9d9a1ccacc47fa96b0b2edbfb5fda4a5215aa
Milo Thurston <knirirr@gmail.com>
2026-06-25 10:19:16 +0100

    Added test for valid FS record #88

---
+++ lib/fair_tests/fm_f4_m_fs_has_fs_record.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+module FmF4MFsHasFsRecord
+  require 'ftr_ruby'
+  require_relative '../fair_test_utils'
+  include FairTestUtils
+
+  def fm_f4_m_fs_has_fs_record(url_record)
+    record = obtain_record_from_text(url_record)
+
+    meta = {
+      testid: 'FM_F4_M_FS_hasFSrecord.ttl',
+      testname: 'FAIR Test - F4 - has FAIRsharing record',
+      description: 'This test assesses whether the identifier being evaluated corresponds to a valid FAIRsharing record. This test expects as input the FAIRsharing DOI or URL for the FAIRsharing record under evaluation.',
+      keywords: ['ARK', 'FAIR', 'F4', 'FAIRsharing'],
+      creator: 'https://orcid.org/0000-0002-6468-9260',
+      indicators: [],
+      metric: 'https://fairsharing.org/8375',
+      license: 'https://creativecommons.org/licenses/by/4.0/',
+      testversion: '1.0.0',
+      protocol: 'https',
+      host: 'ostrails.github.io',
+      basePath: '/assessment-component-metadata-records/test/',
+      endpoint_url: 'https://fair-tests.fairsharing.org/test/',
+      endpoint_description: 'https://fair-tests.fairsharing.org/test_descriptions//api',
+    }
+
+    response = FtrRuby::Output.new(
+      testedGUID: url_record,
+      meta: meta
+    )
+
+
+    if record && !record.empty?
+      response.score = 'pass'
+      response.comments << 'This is a valid FAIRsharing record.'
+    else
+      response.score = 'fail'
+      response.comments << 'No valid FAIRsharing record was found.'
+    end
+
+    response.createEvaluationResponse
+  end
+
+
+end
