a0cea066be63b03945a1988efb2bbe21a50567ac
ramGranell <39826987+ramGranell@users.noreply.github.com>
2026-08-18 15:39:55 +0100

    Update lib/fair_tests/ft_r1_3_m_fs_submission_docs.rb
    Co-authored-by: knirirr <knirirr@users.noreply.github.com>

--- lib/fair_tests/ft_r1_3_m_fs_submission_docs.rb
+++ lib/fair_tests/ft_r1_3_m_fs_submission_docs.rb
@@ -28,7 +28,7 @@ module FtR13MFsSubmissionDocs
       host: 'ostrails.github.io',
       basePath: '/assessment-component-metadata-records/test/',
       endpoint_url: 'https://fair-tests.fairsharing.org/test/ft_r1_3_m_fs_submission_docs',
-      endpoint_description: 'https://fair-tests.fairsharing.org/ft_r1_3_m_fs_submission_docs/api',
+      endpoint_description: 'https://fair-tests.fairsharing.org/test_descriptions/ft_r1_3_m_fs_submission_docs/api',
     }
 
     response = FtrRuby::Output.new(
