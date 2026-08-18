ae6accc3accefab4b828cfbac34ef05d85a3c49e
ramGranell <39826987+ramGranell@users.noreply.github.com>
2026-08-18 15:38:27 +0100

    Update lib/fair_tests/ft_r1_3_m_fs_community_support.rb
    Co-authored-by: knirirr <knirirr@users.noreply.github.com>

--- lib/fair_tests/ft_r1_3_m_fs_community_support.rb
+++ lib/fair_tests/ft_r1_3_m_fs_community_support.rb
@@ -28,7 +28,7 @@ module FtR13MFsCommunitySupport
       host: 'ostrails.github.io',
       basePath: '/assessment-component-metadata-records/test/',
       endpoint_url: 'https://fair-tests.fairsharing.org/test/ft_r1_3_m_fs_community_support',
-      endpoint_description: 'https://fair-tests.fairsharing.org/ft_r1_3_m_fs_community_support/api',
+      endpoint_description: 'https://fair-tests.fairsharing.org/test_descriptions/ft_r1_3_m_fs_community_support/api',
     }
 
     response = FtrRuby::Output.new(
